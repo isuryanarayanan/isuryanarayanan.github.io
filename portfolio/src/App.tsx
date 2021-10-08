db1b3c04d865536506990ef45877b56bd8961890
isuryanarayanan <devdoodan@gmail.com>
2021-10-08 08:00:19 +0530

    changing everything because im braindead

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Canvas } from '@react-three/fiber'
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="App">
+    	<div className="canvas-container">
+				
+			</div>
+    </div>
+  );
+}
+
+export default App;
